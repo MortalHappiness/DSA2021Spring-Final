cd44c15f1f8bfa1e80a5dba2091a7aa689a3130b
jeff-901 <b07901052@ntu.edu.com>
2021-06-05 11:12:33 +0800

    update analyze file

--- src/analyze/tokens_per_user.py
+++ src/analyze/tokens_per_user.py
@@ -1,37 +1,21 @@
-import re
 from collections import Counter
 
+with open("tokens.txt", "r") as fin:
+    tokens = fin.readlines()
+print(len(tokens))
+new_tokens = {}
+l = 15
+aaa = [_[:l] for _ in tokens]
+for token in tokens:
+    token = token.strip()
+    if token[:l] in new_tokens:
+        # print(token)
+        new_tokens[token[:l]].append(token)
+        print(new_tokens[token[:l]])
+    else:
+        new_tokens[token[:l]] = [token]
 
-tokens_cnt = list()
-with open("../testdata/test.in") as fin:
-    next(fin)
-    next(fin)
-    for i in range(10000):
-        L = list()
-        next(fin)
-        next(fin)
-        content = next(fin)
-        subject = next(fin)
-        next(fin)
-        next(fin)
-        content += " " + subject
-        L.append(content)
-        # L.append(subject)
-        # l = 0
-        for s in L:
-            tokens = re.findall("[A-Za-z0-9]+", s)
-            l = len(set(tokens[:]))
-        tokens_cnt.append(l)
-
-# print(tokens_cnt)
-# counter = Counter()
-
-# counter.update(tokens_cnt)
-
-tokens_cnt.sort()
-
-# ========================================
-
-print(f"5 most tokens per mail = {tokens_cnt[-5:]}")
-
-
+# [token[:20] for token in tokens]
+counter = Counter()
+counter.update(aaa)
+print(counter.most_common(20))
