72a0a3cfec8e076185d2a6e1b4253d5f4860224e
jeff-901 <b07901052@ntu.edu.com>
2021-06-11 17:07:00 +0800

    Update

--- src/analyze/tokens_per_user.py
+++ src/analyze/tokens_per_user.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 
 tokens_cnt = list()
+counter = Counter()
+token_2_id={}
+token_2_cnt={}
 with open("../testdata/test.in") as fin:
     next(fin)
     next(fin)
@@ -15,23 +18,62 @@ with open("../testdata/test.in") as fin:
         next(fin)
         next(fin)
         content += " " + subject
-        L.append(content)
+        # L.append(content)
         # L.append(subject)
         # l = 0
-        for s in L:
-            tokens = re.findall("[A-Za-z0-9]+", s)
-            l = len(set(tokens[:]))
-        tokens_cnt.append(l)
+        # for s in L:
+        tokens = re.findall("[A-Za-z0-9]+", content)
+        tokens += re.findall("[A-Za-z0-9]+",  subject)
+        tokens = [token.lower() for token in tokens]
+            # l = len(set(tokens[:]))
+        tokens = set(tokens)
+        for token in tokens:
+            if token in token_2_id:
+                token_2_id[token].append(i)
+            else:
+                token_2_id[token] = [i]
+            if token in token_2_cnt:
+                token_2_cnt[token] +=1
+            else:
+                token_2_cnt[token] = 1
+        counter.update(tokens)
+        # tokens_cnt.append(l)
+set_2_tokens={}
+assert len(token_2_id) == 138078
+for key in token_2_id:
+    token_2_id[key].sort()
+    token_2_id[key] = [str(ele) for ele in token_2_id[key]]
+    # print(" ".join(token_2_id[key]))
+    # print(token_2_id[key])
+    if " ".join(token_2_id[key]) not in set_2_tokens:
+        # print(token_2_id[key])
+        if(len(token_2_id[key]) > 1):
+            set_2_tokens[str(" ".join(token_2_id[key]))] = [key]
+    else:
+        set_2_tokens[" ".join(token_2_id[key])].append(key)
+        # print(set_2_tokens[" ".join(token_2_id[key])])
+cnt = 0
 
+for key in set_2_tokens:
+    if len(set_2_tokens[key]) > 1:
+        # print(set_2_tokens[key], key)
+        cnt += 1
 # print(tokens_cnt)
-# counter = Counter()
-
+# print(token_2_cnt["astika"])
+print(cnt)
 # counter.update(tokens_cnt)
 
-tokens_cnt.sort()
+# tokens_cnt.sort()
 
 # ========================================
-
-print(f"5 most tokens per mail = {tokens_cnt[-5:]}")
+# print(len(counter))
+# cnt = 0
+# for i, ele in enumerate(counter.most_common()):
+#     if(ele[1] == 3):
+#         # print(i)
+#         cnt += 1
+#         # break 
+# print(cnt)
+# print(f"40 most tokens per mail = {counter.most_common()[52326:52328]}")
 
 
