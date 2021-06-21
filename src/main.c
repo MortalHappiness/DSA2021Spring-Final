fca6b32912b9353ce438d3cc24c9dda7647fdd33
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-22 02:25:51 +0800

    Update parameter

--- src/main.c
+++ src/main.c
@@ -76,7 +76,7 @@ int main(void) {
 
     double score = 0;
     for (i = 0; i < n_queries; ++i) {
-        if (queries[i].type == find_similar && queries[i].reward) {
+        if (queries[i].type == find_similar && queries[i].reward > 70) {
             find_similar_query(queries[i].id,
                                queries[i].data.find_similar_data.mid,
                                queries[i].data.find_similar_data.threshold);
